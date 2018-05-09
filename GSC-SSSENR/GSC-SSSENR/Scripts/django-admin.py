7388ea84f65bd178fad5ad10ed3c006e0892a2a3
Daniel L. Young, Ph.D <young.daniel@epa.gov>
2018-05-09 13:01:06 -0400

    Initial

---
+++ GSC-SSSENR/GSC-SSSENR/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\dyoung11\Source\Repos\GSC-SSSENR\GSC-SSSENR\GSC-SSSENR\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ GSC-SSSENR/GSC_SSSENR/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for GSC_SSSENR.
+"""
